e2681d685bf7719291947fb8a8473b896c771dec
Anast9 <102357076+Anast9@users.noreply.github.com>
2023-07-25 14:31:04 +0300

    Обработка Input

---
+++ App.js
@@ -0,0 +1,68 @@
+// logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+import Car from './Car/Car';
+class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'Моя девушка - инопланетянка', year: 2022},
+       {name: 'Глоток молодости', year: 2018},
+       {name: 'Невеста на замену', year: 2019}
+
+      
+    ],
+
+    pageTitle: 'Дорамы смотреть онлайн'
+  }
+
+  changeTitleHandler = (newTitle) =>{
+
+    this.setState({
+      pageTitle: newTitle
+    })
+  }
+
+  handleInput = (event) =>{
+    this.setState({
+      pageTitle: event.target.value
+    })
+  }
+  
+  render() {
+
+    console.log('Render')
+
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    const cars = this.state.cars
+
+    return (
+      <div style={divStyle}>
+        <h1>{this.state.pageTitle}</h1>
+
+       <input type='text' onChange={this.handleInput}/>
+
+
+<button onClick={this.changeTitleHandler.bind(this, 'tecm !')}>Поиск  Дорам</button>
+
+        <Car 
+        name={cars[0].name}
+         year={cars[0].year}
+         onChageTile={this.changeTitleHandler.bind(this, cars[0].name)}
+         />
+        <Car 
+        name={cars[1].name}
+         year={cars[1].year}
+         />
+        <Car 
+        name={cars[2].name}
+         year={cars[2].year}/>
+      </div>
+    );
+  }
+}
+
+export default App;
